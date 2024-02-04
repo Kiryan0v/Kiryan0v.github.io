0dd40abf2d9ed7d3e6ed701bf9874d87f54fc1ba
Виктор <89590301+Kiryan0v@users.noreply.github.com>
2024-02-04 02:50:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from aiogram import Bot, Dispatcher
 
 import asyncio
 
-bot = Bot(token='token')
+bot = Bot(token='6947794055:AAHzYtr6CsPyrFdXrgnQ98tOFOyYRHXFo3M')
 dp = Dispatcher(bot=bot)
 
 async def main():
